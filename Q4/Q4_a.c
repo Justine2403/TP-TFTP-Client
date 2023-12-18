aec896586112eedaa60b9b00a64482502d6fe7ac
Rymouxi <rymouladali@gmail.com>
2023-12-18 11:40:05 +0100

    Update Q4.a

--- Q4/Q4_a.c
+++ Q4/Q4_a.c
@@ -5,7 +5,16 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <arpa/inet.h>  
+#include <errno.h>  
 #define MAX_SIZE 256
+#define RRQ_MODE "octet"
+
+#define ARGUMENTS_ERROR "Error, you must specify 3 arguments."
+#define SERVER_ADDR_ERROR "Error trying to get the server address."
+#define SOCKET_ERROR "Socket creation error"
+#define SEND_ERROR "Error sending data"
+#define RECEIVE_ERROR "Error receiving data"
 
 struct addrinfo hints, *res;
 
@@ -21,13 +30,13 @@ void gettftp(char* host, char* port, char* file) {
     print_message(message);
 }
 
-void getaddr(char* host){
+void getaddr(char* host, char* port, char* file){
     //initializing hints with zeros
     memset(&hints, 0, sizeof(struct addrinfo));
-    hints.ai_family = AF_UNSPEC; // AF_INET or AF_INET6 to force version
-    hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
-
-    int status = getaddrinfo(host, NULL, &hints, &res); 
+    hints.ai_family = AF_INET; // AF_INET or AF_INET6 to force version
+    hints.ai_socktype = SOCK_DGRAM;// UDP datagram 
+    hints.ai_protocol=IPPROTO_UDP;// UDP protocol
+    int status = getaddrinfo(host, port, &hints, &res); 
 
     // check for errors
     if (status != 0) {
@@ -56,25 +65,31 @@ void getaddr(char* host){
     char message[MAX_SIZE];
 
     int ret = snprintf(message, sizeof(message), "Server IP: %s\n", ipstr);
-
+    
+    
     print_message(message); //  print the IP address
-
+   
 
     //reserve socket connexion for the server
     int sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    char sck[MAX_SIZE];
+    snprintf(sck,sizeof(sck),"%d",sockfd);
+    print_message(sck);
+
     if (sockfd == -1) {
         char error[MAX_SIZE];
         snprintf(error, sizeof(error), "ERROR: socket: %s\n", strerror(errno));
         print_message(error);
         exit(EXIT_FAILURE);
     }
+ 
     // Build RRQ packet
-    char rrq_buffer[MAX_BUFFER_SIZE];
+    char rrq_buffer[MAX_SIZE];
     rrq_buffer[0] = 0x00;  // Opcode (2 bytes)
     rrq_buffer[1] = 0x01;
-    strcpy(rrq_buffer + 2, argv[3]);  // Filename
-    strcpy(rrq_buffer + 2 + strlen(argv[3]) + 1, RRQ_MODE);  // 1 byte of zeros + the mode
-    int rrq_length = 2 + strlen(rrq_buffer) + 1 + strlen(argv[3]) + 1;
+    strcpy(rrq_buffer + 2, file);  // Filename
+    strcpy(rrq_buffer + 2 + strlen(file) + 1, RRQ_MODE);  // 1 byte of zeros + the mode
+    int rrq_length = 2 + strlen(rrq_buffer) + 1 + strlen(file) + 1;
 
     // Send the RRQ packet
     if (sendto(sockfd, rrq_buffer, rrq_length, 0, res->ai_addr, res->ai_addrlen) == -1) {
@@ -83,7 +98,8 @@ void getaddr(char* host){
         freeaddrinfo(res);
         exit(EXIT_FAILURE);
         }
-
+    sendto(sockfd, rrq_buffer, rrq_length, 0, res->ai_addr, res->ai_addrlen);
+    
     freeaddrinfo(res); // free the linked list
 }
 
@@ -96,8 +112,7 @@ int main(int argc, char *argv[]) {
         print_message(usage);
         exit(EXIT_FAILURE);
     }
-
-    getaddr(argv[1]);
+    getaddr(argv[1], argv[2], argv[3]);
     gettftp(argv[1], argv[2], argv[3]);
     return 0;
 }
